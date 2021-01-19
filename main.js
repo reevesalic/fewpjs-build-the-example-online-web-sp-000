c325ac9fab88128628c4ab10db14ddb688b7afc3
reevesalic <reevesalic@gmail.com>
2021-01-19 14:57:47 -0500

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector('modal')
+  const hearts = document.getElementsByClassName('like-glyph')
+  // console.log(hearts)
+  
+  for (const heart of hearts) {
+    heart.addEventListener("click", (e) => {
+      
+      mimicServerCall()
+        .then() => {
 
+        if (heart.innerHTML == EMPTY_HEART) {
+        heart.innerHTML = FULL_HEART
+        heart.className = "activated-heart"
+        } else {
+          heart.innerHTML = EMPTY_HEART
+          heart.className = "like-glyph"
+        }
+      })
+      .catch(error => {
+        modal.hidden = true
+        const modalMessage = document.querySelector("#modal-message")
+
+      })
+    })
+  }
+})
+
+// fetch()
+
+}
 
 
 
